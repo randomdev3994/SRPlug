f9a0bb2eeadfa9e9547175676c51ceedce32ff25
randomdev3994 <111250354+randomdev3994@users.noreply.github.com>
2024-06-13 15:05:49 +0100

    Add files via upload

---
+++ hooks/beforebuild.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+const axios = require('axios');
+
+module.exports = function(context) {
+    const projectRoot = context.opts.projectRoot;
+    const wwwDir = path.join(projectRoot, 'www');
+    const outputZipPath = path.join(projectRoot, 'www.zip');
+    const restApiUrl = 'https://danielconceicaodemos-dev.outsystems.app/FileReceiver/rest/SourceAPI/ReceiveSource';
+
+    function createZipFile(sourceDir, outPath){
+        return new Promise((resolve, reject) => {
+            const output = fs.createWriteStream(outPath);
+            const archive = archiver('zip', {
+                zlib: { level: 9 }
+            });
+
+            output.on('close', () => resolve());
+            archive.on('error', err => reject(err));
+
+            archive.pipe(output);
+            archive.directory(sourceDir, false);
+            archive.finalize();
+        });
+    }
+
+    async function uploadZipFile(filePath) {
+        const formData = new FormData();
+        formData.append('file', fs.createReadStream(filePath));
+
+        try {
+            const response = await axios.post(restApiUrl, formData, {
+                headers: {
+                    ...formData.getHeaders()
+                }
+            });
+            console.log('File uploaded successfully:', response.data);
+        } catch(error) {
+            console.error('Error uploading file:', error);
+        }
+    }
+
+    createZipFile(wwwDir, outputZipPath)
+        .then(() => uploadZipFile(outputZipPath))
+        .catch(err => console.error('Error creating or uploading zip file:', err));
+}
