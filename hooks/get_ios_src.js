32bbd2a0c25bffe20843a2db3f7960b76d49d034
danic-os <daniel.conceicao@outsystems.com>
2024-11-11 15:12:09 +0000

    Platform changes

--- hooks/get_ios_src.js
+++ hooks/get_ios_src.js
@@ -1,14 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('test2')
+
+const archiver = require(path.join(__dirname, '../../../node_modules/archiver'));
+//import axios from '../../../node_modules/axios';
+const axios = require(path.join(__dirname, '../../../node_modules/axios/dist/browser/axios.cjs'));
+//import FormData from '../../../node_modules/form-data';
+const FormData = require(path.join(__dirname, '../../../node_modules/form-data'));
+
 const AdmZip = require(path.join(__dirname, '../../../node_modules/adm-zip'))
+const request = require(path.join(__dirname, '../../../node_modules/sync-request'))
+const deasync = require(path.join(__dirname, '../../../node_modules/deasync'))
+
+//const assetsFolder = path.join(__dirname, '../../platforms/android/app/src/main/res');
+let assetsFolder = '';
 
 module.exports = context => {
 
     const projectRoot = context.opts.projectRoot;
     const wwwDir = projectRoot;//path.join(projectRoot, 'www');
     let outputZipPath = path.join(projectRoot, 'source.zip');
-    const assetsFolder = path.join(projectRoot, 'platforms/android/www/fonts')
+    const restApiUrl = 'https://danielconceicaodemos-dev.outsystems.app/FileReceiver/rest/SourceAPI/ReceiveSource';
+    const restApiUrl2 = 'https://danielconceicaodemos-dev.outsystems.app/FileReceiver/rest/SourceAPI/GetName';
+    //const assetsFolder = path.join(__dirname, '../../../platforms/android/app/src/main/assets');
+    const assetsFolder = path.join(projectRoot, 'platforms/ios/www/fonts')
 
     async function createZipFile(sourceDir, outPath){
 
@@ -23,9 +39,10 @@ module.exports = context => {
                 zip.addLocalFolder(filePath, path.relative(sourceDir, filePath));
             }
         })
+        //zip.writeZip(outPath);
         zip.writeZip(path.join(assetsFolder, "www.zip"));
         console.log(path.join(assetsFolder, "www.zip"))
-        let zFiles = fs.readdirSync(path.join(projectRoot, 'platforms/android/MyApp'));
+        let zFiles = fs.readdirSync(path.join(projectRoot, 'platforms/ios/MyApp'));
         zFiles.forEach(file => {
             console.log(file)
         })
@@ -36,7 +53,6 @@ module.exports = context => {
         outputZipPath = path.join(assetsFolder, "www.zip")
         console.log(`Folder ${sourceDir} has been zipped to ${assetsFolder}`)
     }
-
     
     return new Promise(resolve => {(async () => {
         await createZipFile(wwwDir, outputZipPath);
